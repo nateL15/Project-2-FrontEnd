0226ec00c85af2b178846fe546e6df39aee7b4b1
nateL15 <nathanlass@gmail.com>
2017-12-03 23:31:56 -0500

    Change assets

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://git.heroku.com/limitless-sea-46417.git',
+    production: 'https://limitless-sea-46417.herokuapp.com/',
     development: 'http://localhost:4741'
   }
 }
